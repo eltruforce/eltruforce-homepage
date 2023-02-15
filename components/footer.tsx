e34cf8685e142cd431d037922b4981670c7e4172
renatmc <lordace550@gmail.com>
2023-02-14 20:10:06 -0500

    fix(footer): margin top

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mantine/core'
 
 const Footer = () => {
   return (
-    <Box sx={{ textAlign: 'center', fontSize: 14 }} opacity={0.4}>
+    <Box sx={{ textAlign: 'center', fontSize: 14 }} opacity={0.4} mt="xl">
       &copy; {new Date().getFullYear()} Ichiro Macedo. All Rights Reserved.
     </Box>
   )
